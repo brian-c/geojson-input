5c7e0761240d05d5301eda7a720ad67f4c59b9a6
Brian Carstensen <brian.carstensen@gmail.com>
2022-11-13 16:11:47 -0600

    Add a build for the demo page

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	build: {
-		lib: {
-			entry: 'src/geojson-map.ts',
-			formats: ['es']
-		},
-		sourcemap: true,
-	},
-});
+export default defineConfig({});
